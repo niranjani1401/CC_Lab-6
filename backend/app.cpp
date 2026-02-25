4ec2a42fb8429cb77b0f9e878f3914f041f0957f
Niranjani Hiremath <niranjanihiremath@gmail.com>
2026-02-25 19:48:40 +0530

    Make backend run HTTP server on 8080

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main() {
-    while(true) {
-        std::cout << "Backend running..." << std::endl;
-        sleep(5);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    std::cout << "Backend server running on port 8080..." << std::endl;
+
+    while (true) {
+        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        char hostname[1024];
+        gethostname(hostname, 1024);
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Response from container: " + std::string(hostname) + "\n";
+
+        send(new_socket, response.c_str(), response.length(), 0);
+        close(new_socket);
     }
+
     return 0;
 }
 
